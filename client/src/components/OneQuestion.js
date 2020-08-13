bc4fae50fdb0092f0047a5f32e6f48a5041a62ee
Anna Jarocińska <anna.jarocinska5@gmail.com>
2020-08-13 13:56:04 +0200

    Add quiz route

---
+++ client/src/components/OneQuestion.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const OneQuestion = ({ question }) => {
+
+    // let currentNumber = Math.floor(Math.random()*(6-0+1))
+    let currentNumber = 0
+    let currentQuestion = Object.values(question)[currentNumber].newQuestion
+    let currentContinent = Object.values(question)[currentNumber].continent
+    let currentAnswerA = Object.values(question)[currentNumber].answerA
+    let currentAnswerB = Object.values(question)[currentNumber].answerB
+    let currentAnswerC = Object.values(question)[currentNumber].answerC
+    let currentAnswerD = Object.values(question)[currentNumber].answerD
+   
+
+  return (
+            <>
+                {currentQuestion}
+                {currentContinent}
+                {currentAnswerA}
+                {currentAnswerB}
+                {currentAnswerC}
+                {currentAnswerD}
+            </>
+        ) 
+}
+
+export default OneQuestion;
--- client/src/views/Quiz.js
+++ client/src/views/Quiz.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import OneQuestion from '../components/OneQuestion'
 
 const Quiz = () => {
+
+    // const [start, setStart] = useState(false);
+    // const startQuiz = setStart(true);
+
+    const [data, setData] = useState({ question: [] });
+
+    const fetchData = async () => {
+        const result = await axios.get('/api/question')
+        setData(result.data);
+    }
+
+    useEffect(() => {
+        fetchData()
+    }, []);
+
     return (  
         <>
         <p>Quiz</p>
-        <button>Start</button>
+        <button onClick={fetchData}>Start</button>
+        <OneQuestion question={data}/>
         </>
     );
 }
