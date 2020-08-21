9acd697438e894c46d6402326e035a0765e7e580
Anna Jarocińska <anna.jarocinska5@gmail.com>
2020-08-21 12:49:12 +0200

    Add getting random question

--- client/src/game/components/Points.js
+++ client/src/game/components/Points.js
@@ -9,20 +9,27 @@ const Points = ({points, questions, question, getQuestions}) => {
 const questionList = []
 
  Object.entries(questions).map((element) => {
-     console.log(element[1], 'element')
-    Object.values(element[1]).forEach( item => {
-        const oneQuestion = <p>{item}</p>
+    Object.entries(element[1]).forEach( item => {
+        const oneQuestion = <div><button>{item[1]}</button></div>
         questionList.push(oneQuestion)
     })
 })
 
-    
+const randomQuestion = []
+const randomNumber = Math.floor(Math.random()*(3-0));
+const randomQuest = Object.values(questions)[randomNumber]; //obj
+
+for (let prop in randomQuest) {
+    const oneRandomQuestion =<p> {randomQuest[prop]}</p>
+    randomQuestion.push(oneRandomQuestion);
+ }
 
     return ( 
         <>
             <div>    
                 <p>Points: {points}</p>
                 <p>Questions: {questionList}</p>
+                <p>One question: {randomQuestion}</p>
             </div>
         </>
      );
@@ -31,11 +38,11 @@ const questionList = []
 const mapStateToProps = (state) => ({
     points: state.game.points, 
     questions: state.game.questions,
+    // questions: getQuestions(state),
   })
 
   const mapDispatchToProps = dispatch => ({
-      getQuestions: () => dispatch(getQuestions())
-    })
-  
+    getQuestions: () => dispatch(getQuestions())
+  })
 
 export default connect(mapStateToProps, mapDispatchToProps)(Points);
