c109f4ba2ae5bb80f55c06b83fed3836bb0cab51
Anna Jarocińska <anna.jarocinska5@gmail.com>
2020-08-30 22:14:43 +0200

    Implement font awsome

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBinoculars } from '@fortawesome/free-solid-svg-icons';
+import { faCampground } from '@fortawesome/free-solid-svg-icons';
 import Button from '../styles/Button';
 import Container from '../styles/Container';
 import Span from '../styles/Span';
@@ -7,7 +10,8 @@ import Span from '../styles/Span';
 const Navbar = () => {
     return ( 
         <Container nav>
-            <Link to="/moreInforations"> <Span> More informations </Span> </Link>
+            <Link to="/moreInforations"> <Span> <FontAwesomeIcon icon={faBinoculars} /> More informations </Span> </Link>
+            <Link to="/"> <Span> <FontAwesomeIcon icon={faCampground} /> Home </Span> </Link>
             <Container buttons>
                 <Link to="/login"> <Button login> Login </Button> </Link>
                 <Link to="/newUser"> <Button login> Register </Button> </Link>
