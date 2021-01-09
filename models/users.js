c110b39ca626f14888473efd739d650e7d00f029
Anna Jarocińska <anna.jarocinska5@gmail.com>
2021-01-09 22:03:59 +0100

    Delete keys model

--- models/users.js
+++ models/users.js
@@ -2,18 +2,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  id: {
-    type: Number,
-    required: true
-  },
   name: {
     type: String,
     required: true
-  },
-  // email: {
-  //   type: String,
-    // required: true
-  // },
+  }, 
   password: {
     type: String,
     required: true
@@ -25,5 +17,9 @@ const UserSchema = new Schema({
   admin: {
     type: Boolean,
     default: false
+  },
+  key: {
+    type: String,
+    default: ''
   },
 });module.exports = User = mongoose.model("users", UserSchema);
--- routes/newUser.js
+++ routes/newUser.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/users');
-const Keys = require('../models/keys');
 const bcrypt = require('bcryptjs');
 
 router.post('/', async (req, res) => {
@@ -11,17 +10,15 @@ router.post('/', async (req, res) => {
                 res.send('rejection')
             }
             if (!userNameNotAvailable) {
-              res.send('created')
               const salt = await bcrypt.genSalt(10);
-              let userid = 3
               let username = req.body.name
               let hashedPassword = await bcrypt.hash(req.body.password, salt);
               const newUser = new User({
-                id: userid,
                 name: username,
                 password: hashedPassword,
               });
               newUser.save()
+              res.send('created')
             }
 });
 
