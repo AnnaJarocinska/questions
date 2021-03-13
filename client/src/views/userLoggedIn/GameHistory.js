752c1764381b52b9c3e07d714fca91b577276677
Anna Jarocińska <anna.jarocinska5@gmail.com>
2021-03-13 21:20:35 +0100

    Display games details

--- client/src/views/userLoggedIn/GameHistory.js
+++ client/src/views/userLoggedIn/GameHistory.js
@@ -17,12 +17,25 @@ const GameHistory = () => {
         fetchData()
     }, []);
 
-    console.log(data, 'data w gamehistory')
+    let list = [];
+     if (data[0] !== undefined) {
+        for (let i=0; i< data.length; i++){
+            for (const key in data[i]){
+                console.log(key, data[i][key])
+                list.push(
+                <>
+                <p>{key} : {data[i][key]}</p>
+                </>
+                )
+                ;
+            }
+        }}
     const gamesNumber = data.length
     return (  
         <Container separate users> 
             <CapitalLetterParagraph> Game history</CapitalLetterParagraph>
             <Paragraph>Games number: {gamesNumber}</Paragraph>
+            {list}
         </Container>
     );
 }
