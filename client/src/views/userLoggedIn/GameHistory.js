cacd354cb303bf20d3117e79f37c0cf01fcc39e7
Anna Jarocińska <anna.jarocinska5@gmail.com>
2021-03-14 22:52:26 +0100

    Refine games details list

--- client/src/views/userLoggedIn/GameHistory.js
+++ client/src/views/userLoggedIn/GameHistory.js
@@ -23,11 +23,13 @@ const GameHistory = () => {
         for (let i=0; i< gamesNumber; i++){
             let item = [];
             for (const key in data[i]){
-                item.push(
-                    <li> <Paragraph>{key} : {data[i][key]}</Paragraph> </li>
-                )
-                ;
-            }
+                if (key !== "_id"){
+                    let value = key !== "date" ? data[i][key] : data[i][key].substr(0,10);
+                    let keyName = key.indexOf("Answers") !== -1 ? key.replace("Answers", " answers") : key; 
+                    item.push(
+                        <li> <Paragraph>{keyName} : {value}</Paragraph> </li>
+                    );
+            }}
             list.push(<Section><ul>{i+1}{item}</ul></Section>)
         }}
     return (  
