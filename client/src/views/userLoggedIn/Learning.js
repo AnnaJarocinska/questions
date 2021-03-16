ff2beca9d7df890b79befc4e7e375fc93e3248fb
Anna Jarocińska <anna.jarocinska5@gmail.com>
2021-03-16 14:45:44 +0100

    Create a all capitals list draft

--- client/src/views/userLoggedIn/Learning.js
+++ client/src/views/userLoggedIn/Learning.js
@@ -9,33 +9,105 @@ const Learning = () => {
 
   const [data, setData] = useState({ capitals: [] });
 
-    const fetchData = async () => {
-        const result = await axios.post('/api/capitalsList')
-        setData(result.data);
+  const fetchData = async () => {
+      const result = await axios.post('/api/capitalsList')
+      setData(result.data);
     }
 
-    useEffect(() => {
-        fetchData()
+  useEffect(() => {
+      fetchData()
     }, []);
 
     let list = [];
+    let africa = [];
+    let australia = [];
+    let asia = [];
+    let europe = [];
+    let northAmerica = [];
+    let southAmerica = [];
+
     for (let i=0; i<data.length; i++){
         let country = data[i].question.slice(28);
         let letter = data[i].goodAnswer.toUpperCase();
         let answer = `answer${letter}`;
         let capitalCity = data[i][answer];
         let continent = data[i].continent;
-        let continentSection = [];
         
-        if(continent === "Europe"){ 
-        continentSection.push(
-          <Section>
+        if(continent === "Africa"){ 
+          africa.push(
+          <>
             <Paragraph center> {country} </Paragraph>
             <CapitalLetterParagraph> {capitalCity} </CapitalLetterParagraph>
-          </Section>)
+          </>
+          )}
+
+        if(continent === "Asia"){ 
+          asia.push(
+            <>
+              <Paragraph center> {country} </Paragraph>
+              <CapitalLetterParagraph> {capitalCity} </CapitalLetterParagraph>
+            </>
+            )}
+
+        if(continent === "Australia"){ 
+          australia.push(
+            <>
+              <Paragraph center> {country} </Paragraph>
+              <CapitalLetterParagraph> {capitalCity} </CapitalLetterParagraph>
+            </>
+              )}
+
+        if(continent === "Europe"){ 
+            europe.push(
+              <>
+                <Paragraph center> {country} </Paragraph>
+                <CapitalLetterParagraph> {capitalCity} </CapitalLetterParagraph>
+              </>
+              )}
+              
+        if(continent === "North America"){ 
+          northAmerica.push(
+            <>
+              <Paragraph center> {country} </Paragraph>
+              <CapitalLetterParagraph> {capitalCity} </CapitalLetterParagraph>
+            </>
+              )}
+
+        if(continent === "South America"){ 
+          southAmerica.push(
+            <>
+              <Paragraph center> {country} </Paragraph>
+              <CapitalLetterParagraph> {capitalCity} </CapitalLetterParagraph>
+            </>
+              )}
+        }
   
-  list.push(continentSection)
-        }}
+  list.push(
+    <Section>
+      {africa}
+    </Section>,
+
+    <Section>
+      {asia}
+    </Section>,
+
+    <Section>
+      {australia}
+    </Section>,
+       
+    <Section>
+      {europe}
+    </Section>,
+          
+    <Section>
+      {northAmerica}
+    </Section>,
+         
+    <Section>
+      {southAmerica}
+    </Section>
+    )
+        
     return (
         <Container separate>
           <Paragraph>Capitals list</Paragraph>
