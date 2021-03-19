967440e5fa6c97ab4e8f65d7bf2d9cc9d936c672
Anna Jarocińska <anna.jarocinska5@gmail.com>
2021-03-19 06:59:50 +0100

    Fix capitals list

--- client/src/views/userLoggedIn/Learning.js
+++ client/src/views/userLoggedIn/Learning.js
@@ -9,8 +9,7 @@ import Badge from '../../styles/Badge';
 const Learning = () => {
 
   const [data, setData] = useState({ capitals: [] });
-  const [continents, setContinents] = useState(
-    ['Africa','Asia', 'Australia','Europe', 'North America', 'South America']);
+  const continents = ['Africa','Asia', 'Australia','Europe', 'North America', 'South America'];
   const fetchData = async () => {
       const result = await axios.post('/api/capitalsList')
       setData(result.data);
@@ -20,39 +19,38 @@ const Learning = () => {
       fetchData()
     }, []);
 
-    let list = [];
-    let continentsList = {
-    africa : [<Badge continent = 'Africa'>{'Africa'}</Badge>],
-    australia : [<Badge continent = 'Australia'>{'Australia'}</Badge>],
-    asia : [<Badge continent = 'Asia'>{'Asia'}</Badge>],
-    europe : [<Badge continent = 'Europe'>{'Europe'}</Badge>],
-    northAmerica : [<Badge continent = 'North America'>{'North America'}</Badge>],
-    southAmerica : [<Badge continent = 'South America'>{'South America'}</Badge>]
-    }
-      
-        for (let i=0; i<data.length; i++) { 
-          let country = data[i].question.slice(28);
-          let letter = data[i].goodAnswer.toUpperCase();
-          let answer = `answer${letter}`;
-          let capitalCity = data[i][answer];
-          let continentName = data[i].continent;
-          let firstLetter = continentName.charAt(0);
-          let continent = continentName.replace(" ", "").replace(firstLetter, firstLetter.toLowerCase());
+  let list = [];
+    
+  for (let i=0; i<continents.length; i++) { 
+    let cont = continents[i];
+    let rr = cont.replace(" ", "").replace(cont.charAt(0), cont.charAt(0).toLowerCase());
+    let continentsList = {[`${rr}`] : [<Badge continent = {cont}>{cont}</Badge>]}; 
+    
+      for (let i=0; i<data.length; i++) { 
+        let country = data[i].question.slice(28);
+        let letter = data[i].goodAnswer.toUpperCase();
+        let answer = `answer${letter}`;
+        let capitalCity = data[i][answer];
+        let continentName = data[i].continent;
+        let firstLetter = continentName.charAt(0);
+        let continent = continentName.replace(" ", "").replace(firstLetter, firstLetter.toLowerCase());
          
+        if(continentsList[continent]!==undefined){
           continentsList[continent].push(
-              <>
-                <Paragraph center> {country} </Paragraph>
-                <CapitalLetterParagraph> {capitalCity} </CapitalLetterParagraph>
-              </>
-              )
+            <>
+              <Paragraph center> {country} </Paragraph>
+              <CapitalLetterParagraph> {capitalCity} </CapitalLetterParagraph>
+            </>
+          )
         }
-        for (const value of Object.values(continentsList)) {
-          list.push(
-            <Section>
-              {value}
-            </Section>)
+      }  
+      for (const value of Object.values(continentsList)) {
+        list.push(
+          <Section>
+            {value}
+          </Section>)
       }
-  
+    }
     return (
         <Container separate user>
           <Paragraph>Capitals list</Paragraph>
@@ -60,5 +58,5 @@ const Learning = () => {
         </Container>
       );
 }
- 
+
 export default Learning;
