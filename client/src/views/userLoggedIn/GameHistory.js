2157c61b801ce04a5d86cbac7ce8e8e3a202aec3
Anna Jarocińska <anna.jarocinska5@gmail.com>
2021-03-22 21:13:15 +0100

    Add keys to GameHistory.js

--- client/src/views/userLoggedIn/GameHistory.js
+++ client/src/views/userLoggedIn/GameHistory.js
@@ -4,6 +4,7 @@ import Container from '../../styles/Container';
 import CapitalLetterParagraph from '../../styles/CapitalLetterParagraph';
 import Paragraph from '../../styles/Paragraph';
 import Section from '../../styles/Section';
+import { v4 as uuidv4 } from 'uuid';
 
 const GameHistory = () => {
 
@@ -27,10 +28,10 @@ const GameHistory = () => {
                     let valueName = key !== "date" ? data[i][key] : data[i][key].substr(0,10);
                     let keyName = key.indexOf("Answers") !== -1 ? key.replace("Answers", " answers") : key; 
                     item.push(
-                        <li> <Paragraph>{keyName}: {valueName}</Paragraph> </li>
+                        <li key={uuidv4()}> <Paragraph>{keyName}: {valueName}</Paragraph> </li>
                     );
             }}
-            list.push(<Section><ul>{i+1}{item}</ul></Section>)
+            list.push(<Section key={uuidv4()}><ul>{i+1}{item}</ul></Section>)
         }}
     return (  
         <Container separate left user> 
