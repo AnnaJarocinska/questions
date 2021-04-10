c737bc01a0ab9816b97ba91cf0ddd957b30ef0fa
Anna Jarocińska <anna.jarocinska5@gmail.com>
2021-04-10 22:05:54 +0200

    Change password

--- routes/user.js
+++ routes/user.js
@@ -5,104 +5,62 @@ const bcrypt = require('bcryptjs');
 const User = require('../models/users');
 
 router.all('/', verifyUser, async (req, res) => {
-
-    try {
+  try {
       const isUser = await User.findOne({key: req.cookies.key});
-      if(isUser && !isUser.admin){
+      if (isUser && !isUser.admin) {
         res.send({userName: isUser.name, created: isUser.created, admin: isUser.admin})
-      } else { return res.status(401).send({message: 'rejection'})
+      } else { 
+        return res.status(401).send({message: 'rejection'})
       }
-    } catch (err) {
-        res.status(401).json({
-          err
-      });
+  } catch (err) {
+      res.status(401).json({err});
       }
-  })
+  });
 
-  // router.get('/game', verifyUser, async (req, res, next) => {
-  //  try{
-  //    const allQuestions = await Question.find({});
-  //    res.send(allQuestions)
-  //  }catch (err) {
-  //   res.status(401).json({
-  //     err
-  // });
-  // }
-  // });
-
-  router.post('/saveGame', async (req, res, next) => {
-    try{
-      const user = await User.findOne({key: req.cookies.key});
-      let obj = {
+router.post('/saveGame', async (req, res, next) => {
+  try {
+    const user = await User.findOne({key: req.cookies.key});
+    let obj = {
                 points: req.body.points,
                 category: req.body.category,
                 correctAnswers: req.body.correctAnswers,
                 wrongAnswers: req.body.wrongAnswers,
                 date: Date.now(),
               }
-      user.games.push(obj)
-      user.save()
-    } catch (err) {
-     res.status(401).json({
-       err
-   });
+    user.games.push(obj)
+    user.save()
+  } catch (err) {
+    res.status(401).json({err});
    }
-   });
+  });
 
-   router.post('/history', async (req, res, next) => {
-    try{
-      const user = await User.find({key: req.cookies.key});
-      res.send(user[0].games)
-    } catch (err) {
-     res.status(401).json({
-       err
-   });
+router.post('/history', async (req, res, next) => {
+  try {
+    const user = await User.find({key: req.cookies.key});
+    res.send(user[0].games)
+  } catch (err) {
+    res.status(401).json({err});
    }
-   });
+});
 
-   router.post('/details/changePassword', async (req, res, next) => {
-    try{
-      const salt = await bcrypt.genSalt(10);
-        let newPassword = await   bcrypt.hash(req.body.password, salt);
-        const filter = { key: req.cookies.key };
-const update = { password: newPassword };
-let trr = await User.findOneAndUpdate(filter, update);
-trr()
-      // const user =  User.findOneAndUpdate({'key': req.cookies.key},
-        // { 'password': newPassword},
-    // { 'new': true  },
-        // function(err, doc){
-          if (err) {
-            
-            console.log('ERROR');
-        } else {
-            console.log('success');
-        //     res.status(200).send(updatedUser)
+router.post('/details/changePassword', async (req, res, next) => {
+  try{
+    const salt = await bcrypt.genSalt(10);
+        // let newPassword = await bcrypt.hash(req.body.password, salt);
+    let newPassword = req.body.password;
+    const filter = { key: req.cookies.key };
+    const update = { password: newPassword };
+    let change = await User.findOneAndUpdate(filter, update);
+    change();
+    if (err) {
+      console.log('ERROR');
+    } else {
+      console.log('success');
         }
-        // const salt = bcrypt.genSalt(10);
-        // let newPassword = bcrypt.hash(req.body.password, salt);
-        // let obj = {password: newPassword};
-        // user.save({obj});
-      // });
-      // const query = {key: req.cookies.key} 
-      console.log(req.body.password, 'req.body.password');
-      // const salt = await bcrypt.genSalt(10);
-      console.log(user[0].password, 'user[0].password PRZED')
-      // let newPassword = await bcrypt.hash(req.body.password, salt);
-      // user.password = newPassword;
-      // user.save();
-     console.log(user[0].password, 'user[0].password PO')
-      res.send('userpassword')
     } 
-    catch (err) {
-     res.status(401).json({
-       err
+  catch (err) {
+     res.status(401).json({err})
    }
-   )
-   
-   }
-   });//
-
-
+});
 
 module.exports = router;
