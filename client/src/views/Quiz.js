72c015f90c65413b5f689921189034e248807704
Anna Jarocińska <anna.jarocinska5@gmail.com>
2021-04-12 22:49:25 +0200

    Add Container to Quiz

--- client/src/views/Quiz.js
+++ client/src/views/Quiz.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import RealGameFetchQuestions from '../real game/components/RealGameFetchQuestions';
 import realGameActions from '../real game/duck/actions';
+import Container from '../styles/Container';
 
 const Quiz = ({setGameDetails}) => {
     const category = ['Africa','Asia', 'Australia','Europe', 'North America', 'South America'];
@@ -9,10 +10,9 @@ const Quiz = ({setGameDetails}) => {
     const toSave = 'no';
     setGameDetails(category, mode)
     return (  
-        <>
-            <p>quick game</p>
+        <Container main>
             <RealGameFetchQuestions category={category} mode={mode} toSave={toSave}/>
-        </>
+        </Container>
     );
 }
 const mapDispatchToProps = (dispatch) => ({
