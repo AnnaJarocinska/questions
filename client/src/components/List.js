79fe5dd9efaa3a7392d288daac76ba25a821018b
Anna Jarocińska <anna.jarocinska5@gmail.com>
2021-05-25 21:26:18 +0200

    Update

--- client/src/components/List.js
+++ client/src/components/List.js
@@ -14,16 +14,31 @@ const List = ({ questions, deleteQuestion }) => {
           questions.length > 0 ?
           (
             questions.map(question => {
+              let goodAnswer = question.goodAnswer.toUpperCase();
+              let variants = ['A', 'B', 'C', 'D'];
+              let answer=[];
+             
+              for (let i = 0; i<variants.length; i++){
+                return console.log(variants[i], 'bbb')
+                // answer.push(
+                //   <Paragraph>
+                //     {`${question.answer+variants[i]}`.includes(goodAnswer) ? 
+                //     `${question.answervariants[i]}.toUpperCase()}` :
+                //      `${question.answervariants[i]}` }
+                //      </Paragraph>)
+              
+              }
               return (
                 <Container list="true" key={uuidv4()}>
                   <li key={question._id} onClick={() => deleteQuestion(question._id)}>
                   <Paragraph>{question.question}</Paragraph>
                   <Badge continent={question.continent}>{question.continent}</Badge>
-                  <Paragraph>{question.answerA}</Paragraph>
-                  <Paragraph>{question.answerB}</Paragraph>
-                  <Paragraph>{question.answerC}</Paragraph>
-                  <Paragraph>{question.answerD}</Paragraph>
-                  <Paragraph> {question.goodAnswer}</Paragraph>
+                  {answer}
+                  {/* <Paragraph>{question.answerA.includes(question.goodAnswer.toUpperCase()) ? question.answerA.toUpperCase() : question.answerA }</Paragraph>
+                  <Paragraph>{question.answerB.includes(question.goodAnswer.toUpperCase()) ? question.answerB.toUpperCase() : question.answerB }</Paragraph>
+                  <Paragraph>{question.answerC.includes(question.goodAnswer.toUpperCase()) ? question.answerC.toUpperCase() : question.answerC }</Paragraph>
+                  <Paragraph>{question.answerD.includes(question.goodAnswer.toUpperCase()) ? question.answerD.toUpperCase() : question.answerD }</Paragraph> */}
+                  <Paragraph> {goodAnswer}</Paragraph>
                 </li>
                 </Container>
               )
